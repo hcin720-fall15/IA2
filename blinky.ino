dbae0e827d0be044cfc74af21c5df2ce9c25e9da
Daniel Ashbrook <daniel.ashbrook@rit.edu>
2015-09-09 17:06:22 -0400

    added comments, Spark.publish()

--- blinky.ino
+++ blinky.ino
@@ -1,17 +1,24 @@
-SYSTEM_MODE(SEMI_AUTOMATIC); //Disable connecting to cloud
-
 void setup()
 {
 	pinMode(D7, OUTPUT);
 	Serial.begin(9600);
 }
-
 void loop()
 {
+	//Turn the LED on
 	digitalWrite(D7, HIGH);
+
+	//Send a message to the computer over the serial port
 	Serial.println("Turned LED on!");
+
+	//Send a message to the cloud
+	Spark.publish("LEDEvent", "on");
+
+	//Wait 2 seconds
 	delay(2000);
+
 	digitalWrite(D7, LOW);
 	Serial.println("Turned LED off!");
+	Spark.publish("LEDEvent", "off");
 	delay(1000);
 }
